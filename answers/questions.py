3f2ed425937582f15789402b8bc50e6336a79d72
immijimmi <immijimmi1@gmail.com>
2021-10-21 05:11:50 +0100

    Corrected method name

--- answers/questions.py
+++ answers/questions.py
@@ -2,7 +2,7 @@ from abc import ABC
 
 
 class Questions(ABC):
-    def test_get_and_capitalise_second_letter_of_string(self, string):
+    def get_and_capitalise_second_letter_of_string(self, string):
         """
         This function should receive a string at least 2 characters long,
         and return the capital of the second character
